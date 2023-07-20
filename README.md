debff3868ca97ab33c4fe5a7bb0e82ef1ff03672
Evan Clough <evanclough99@gmail.com>
2023-07-20 11:25:54 -0400

    added example of how to use the linear regression class

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # wheels
 A C++ library with basic neural network functionality from the ground up to teach myself the fundamentals. "Re-inventing the wheel".
+
+## Usage
+
+### Compilation: 
+For now, you can just do 
+``` g++ *.cpp -o output```
+
+### Datasets
+You can create a Dataset via vectors directly created in code, or from a specified CSV, with specified columns to use as features/labels. View the constructors for more info.
+
+### Linear Regression
+You can create a linear regression model with a specified model name, training dataset, and test dataset. You can train the model with the train_model function, with a specified learning rate, number of epochs, and validation split. You can test the model against the test dataset with the test_model.
---
+++ linear_regression_example/main.cpp
@@ -0,0 +1,23 @@
+#include "../Linear_Regression.h"
+
+//demonstration of the basic functions of the linear regression class of the library.
+//for now, you can compile it with g++ *.cpp ..\*.cpp
+
+int main() {
+    //first initialize our training dataset, we'll do it from the the my_data csv
+    //we're going to want to capture apples and bananas as our feature columns, and oranges as our label column.
+    std::unique_ptr<Dataset> training_ds = std::make_unique<Dataset>(Dataset("fruit_data.csv", {"apples", "bananas"}, "oranges"));
+
+    //then initialize our test dataset, we're going to do it directly in code.
+    std::unique_ptr<Dataset> test_ds = std::make_unique<Dataset>(Dataset(std::vector<std::vector<float>>({{1, 2}}), {"apples", "bananas"},  std::vector<float>({5})));
+
+    //then we can initialize our model wtih both of our datasets
+    Linear_Regression my_model("Fruits", 2, std::move(training_ds), std::move(test_ds));
+    
+    //then we can train our model with the train_model method. we're going to see how the number of apples and bananas someone has correlates with the number of oranges.
+    //the train_model function takes in a learning rate, number of epochs to run, and validation split. 
+    my_model.train_model(0.01, 100, 0.2);
+
+    //we can then test our model against our test dataset to see how well it does.
+    my_model.test_model();
+}
